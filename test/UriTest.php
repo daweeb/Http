5aaf442737e97211949557850b3a72af297af442
Laurenz Gass <gass@b1-systems.de>
2022-01-13 12:26:22 +0000

    added tests for Port Host and Fragment

--- test/UriTest.php
+++ test/UriTest.php
@@ -52,4 +52,76 @@ class UriTest extends TestCase
         $uri = new Uri($url);
         $this->assertEquals($url, (string) $uri);
     }
+
+    public function testWithValidPort()
+    {
+        $port = '56564';
+        $uri = new Uri();
+        $uri = $uri->withPort($port);
+        $this->assertEquals($uri->getPort(),$port);
+    }
+
+    /** Port outside port range 1-65535 InvalidArgumentException expected.
+     * No error is thrown?
+     */ 
+
+    public function testWithInvalidPort()
+    {
+        $port = '65536';
+        $uri = new Uri();
+        $uri = $uri->withPort($port);
+        $this->assertEquals($uri->getPort(), $port);
+    }
+
+    public function testWithNullPort()
+    {
+        $port = '';
+        $uri = new Uri();
+        $uri = $uri->withPort($port);
+        $this->assertEquals($uri->getPort(), null);
+    }
+
+    public function testWithValidLowerHost()
+    {
+        $host = 'groupware';
+        $uri = new Uri();
+        $uri = $uri->withHost($host);
+        $this->assertEquals($uri->getHost(), $host);
+    }
+
+    public function testWithValidUpperHost()
+    {
+        $host = 'GroupWare';
+        $uri = new Uri();
+        $uri = $uri->withHost($host);
+        $this->assertEquals($uri->getHost(), 'groupware');
+    }
+
+    //  no exception thrown?
+    //  @throws \InvalidArgumentException for invalid hostnames.
+    public function testWithInvalidHost()
+    {
+        $host = 'group@ware';
+        $uri = new Uri();
+        $uri = $uri->withHost($host);
+        $this->assertEquals($uri->getHost(), $host);
+    }
+
+    public function testWithNullHost()
+    {
+        $host = '';
+        $uri = new Uri();
+        $uri = $uri->withHost($host);
+        $this->assertEquals($uri->getHost(), null);
+    }
+
+    public function testWithFragment()
+    {
+        $path = '/hello/world';
+        $fragment = '#print';
+        $uri = new Uri();
+        $uri = $uri->withPath($path . $fragment);
+        $this->assertEquals($fragment->getFragment(),$fragment);
+    }
 }
+
